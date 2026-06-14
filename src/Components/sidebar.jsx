62884900dc6812f1a3edca381ee7d4b8a0878457
ShanAli <shanali@gmail.com>
2026-06-14 12:49:16 +0500

    sider bar

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,16 +1,20 @@
-import { ChartArea, HomeIcon, PiggyBankIcon, SettingsIcon } from 'lucide-react'
+import { ChartArea, HomeIcon, LucideDollarSign, Mail, MessageCircleDashed, PersonStanding, PersonStandingIcon, PiggyBankIcon, SettingsIcon, TriangleDashedIcon } from 'lucide-react'
 import React from 'react'
 
 function Sidebar() {
     return (
-        <div className=' w-16 border bg-purple-500 h-155 flex flex-col items-center '>
-            <div className=' text-white font-bold border w-12  '>dappr</div>
-            <div>
-                <HomeIcon/>
-                <ChartArea/>
-                <HomeIcon/>
+        <div className=' w-16  bg-purple-500 h-155 flex flex-col items-center justify-center gap-4  '>
+            <div className=' font-[serif] text-white font-bold  w-12 h-20  '>dappr</div>
+            <div className=' flex flex-col  gap-8 p-3 h-110'>
+                <HomeIcon color='white'/>
+                <ChartArea color='white'/>
+                <HomeIcon color='white'/>
+                <Mail color='white'/>
+                <PersonStandingIcon color='white'/>
+                <ChartArea color='white'/>
+                <LucideDollarSign color='white'/>
             </div>
-            <div className=' border '>
+            <div className='  '>
                 <SettingsIcon color='white'/>
             </div>
         </div>
